e160cc84063e0602637863f64b313a19f0b5f130
dr. Simon Bálint <balladin0@gmail.com>
2020-10-28 22:14:26 +0100

    Add Q16.2 solution for multiple runs

--- question_16.2/src/main/java/com/codecool/WordFrequency.java
+++ question_16.2/src/main/java/com/codecool/WordFrequency.java
@@ -8,6 +8,8 @@ package com.codecool;
 /* Notes
 * */
 
+import java.util.HashMap;
+
 public class WordFrequency {
     public int getWordFrequencyOnce(String[] book, String wordToFind) {
         if (book == null || wordToFind == null) return -1;
@@ -20,6 +22,28 @@ public class WordFrequency {
         }
 
         return count;
+    }
+
+    // What if we were running this algorithm multiple times?
+
+    public HashMap<String, Integer> setupBookDictionary(String[] book) {
+        HashMap<String, Integer> bookWords = new HashMap<>();
+        for (String word : book) {
+            word = word.trim().toLowerCase();
+            if (!bookWords.containsKey(word)) {
+                bookWords.put(word, 1);
+            } else {
+                bookWords.put(word, bookWords.get(word) + 1);
+            }
+        }
+        return bookWords;
+    }
+
+    public int getWordFrequencyMultipleTimes(HashMap<String, Integer> bookWords, String wordToFind) {
+        if (bookWords == null || wordToFind == null) return -1;
+        wordToFind = wordToFind.trim().toLowerCase();
+        if (bookWords.containsKey(wordToFind)) return bookWords.get(wordToFind);
+        return 0;
     }
 
 }
